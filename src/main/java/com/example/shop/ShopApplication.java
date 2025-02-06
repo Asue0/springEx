0afffa78264ab7f88d536664f8c30c8d7b8e5e18
김지호 <divefreedom@naver.com>
2025-02-06 14:10:15 +0900

    젠킨스 깃허브 웹 훅 확인용 진진)

--- src/main/java/com/example/shop/ShopApplication.java
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-	// 뻘 주석 추가 진)
+	// 뻘 주석 추가 진진)
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
